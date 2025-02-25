36aae1b90e906c3c635e0129da574b03053aede8
Syed Fatik Islam <89350166+fatik-islam@users.noreply.github.com>
2025-02-25 12:57:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
 class Config:
-    # Workday credentials and tenant information
-    WORKDAY_USERNAME = os.getenv("WORKDAY_USERNAME", "your_username")
-    WORKDAY_PASSWORD = os.getenv("WORKDAY_PASSWORD", "your_password")
-    WORKDAY_TENANT   = os.getenv("WORKDAY_TENANT", "your_tenant")
-
-    # WSDL URL for the Workday SOAP API
-    WORKDAY_WSDL = os.getenv(
-        "WORKDAY_WSDL", 
-        "https://wd2-impl-services1.workday.com/ccx/service/ibmsrv_pt1/Human_Resources/v43.2?wsdl"
-    )
-
+    WORKDAY_WSDL = os.getenv("WORKDAY_WSDL", "https://wd2-impl-services1.workday.com/ccx/service/ibmsrv_pt1/Human_Resources/v43.2?wsdl")
+    WORKDAY_TENANT = os.getenv("WORKDAY_TENANT", "your_workday_tenant")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
